7fa93a008a74dfa0337fec854873ad1d6bce63ce
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-05-26 10:20:59 +0700

    Update open webcam.py

--- open webcam.py
+++ open webcam.py
@@ -1,9 +1,9 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) # 0 = default webcam
 
 while True:
-    ret, frame = cap.read()
+    ret, frame = cap.read() # Capture frame
     if not ret:
         break
     cv2.imshow("Webcam", frame)
