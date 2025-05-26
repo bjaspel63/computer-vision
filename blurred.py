c064aa3ffc94086bad6cc20d9ffb961269fe1c8c
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-05-26 10:18:44 +0700

    Update blurred.py

--- blurred.py
+++ blurred.py
@@ -2,7 +2,8 @@ import cv2
 
 img = cv2.imread("sample.jpg")     # Load an image
 
-blurred = cv2.GaussianBlur(img, (7, 7), 0)
+# Apply Gaussian Blur to smooth the image
+blurred = cv2.GaussianBlur(img, (7, 7), 0) #7,7 = Size of the blur area (must be odd numbers); 0 =Auto-calculate the blur strength.
 cv2.imshow("Blurred Image", blurred)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
