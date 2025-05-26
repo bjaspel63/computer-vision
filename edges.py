8b0cc18eaf8b4c81b8dff2e7b7689aa83a14b378
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-05-26 10:20:04 +0700

    Update edges.py

--- edges.py
+++ edges.py
@@ -2,7 +2,7 @@ import cv2
 
 img = cv2.imread("sample.jpg")     # Load an image
 
-edges = cv2.Canny(img, 100, 200)
+edges = cv2.Canny(img, 100, 200) #Finds edges (lines and boundaries); 100, 200: Lower and upper edge detection thresholds
 cv2.imshow("Edges", edges)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
