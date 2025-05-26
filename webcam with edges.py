f8b4227441d3ca7eb38d24d2bdfd01fcc8f3c0bb
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-05-26 10:22:19 +0700

    Update webcam with edges.py

--- webcam with edges.py
+++ webcam with edges.py
@@ -1,17 +1,17 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)  # 0 = default webcam
 
 while True:
-    ret, frame = cap.read()
+    ret, frame = cap.read() # Capture frame
     if not ret:
         break
-    edges = cv2.Canny(frame, 100, 200)
-    cv2.imshow("Edge Filter", edges)
+    edges = cv2.Canny(frame, 100, 200) # Apply edge filter
+    cv2.imshow("Edge Filter", edges) # Show it
 
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'): # Waits 1 ms; checks if 'q' was pressed
         break
 
-cap.release()
+cap.release() #Turns off webcam
 cv2.destroyAllWindows()
