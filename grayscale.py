695b65b640529a2d3d003edb24e58187360fc169
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-05-26 10:15:05 +0700

    Update grayscale.py

--- grayscale.py
+++ grayscale.py
@@ -1,7 +1,9 @@
-import cv2
+import cv
 
-img = cv2.imread("sample.jpg")     # Load an image
+# Convert the image to black and white (grayscale)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Show the grayscale image
 cv2.imshow("Grayscale", gray)
-cv2.waitKey(0)                     # Wait for key press
-cv2.destroyAllWindows()           # Close the window
+cv2.waitKey(0)
+cv2.destroyAllWindows()
